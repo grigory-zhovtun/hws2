39fee65431f65ca49ab6850645bb8a27a065875f
grigory-zhovtun <grigory.zhovtun@gmail.com>
2024-10-17 23:07:32 +0500

    done homework second (affairs)

--- src/s2-homeworks/hw01/HW1.tsx
+++ src/s2-homeworks/hw01/HW1.tsx
@@ -13,8 +13,21 @@ import avatar from './avatar.png'
 * 5 - сделать стили в соответствии с дизайном
 * */
 
+type UserType = {
+    avatar: string
+    name: string
+}
+type MsgType = {
+    text: string
+    time: string
+}
+
 // нужно создать правильный тип вместо any
-export type MessageType = any
+export type MessageType = {
+    id: number
+    user: UserType
+    message: MsgType
+}
 
 // структуру объекта не менять
 export const message0: MessageType = {
